86b499a6f098c62717060c54bf3b8ad7ea341dfc
Samin-Hosseini <Samin.hosseinii80@gmail.com>
2024-12-18 15:49:05 +0330

    finish

--- javascript/j4.js
+++ javascript/j4.js
@@ -1,7 +1,9 @@
 const randomArray = () => {
   let array = Array.from({ length: 10 }, () => Math.floor(Math.random() * 11));
-  let sortedArray1 = array.sort((a, b) => a - b);
-  let sortedArray2 = array.sort((a, b) => b - a);
+  // مرتب سازی کوچک به بزرگ
+  let sortedArray1 = array.flat().sort((a, b) => a - b);
+  // مرتب سازی بزرگ به کوچک
+  let sortedArray2 = array.flat().sort((a, b) => b - a);
   console.log(sortedArray1);
   console.log(sortedArray2);
 };
